eb31ce92910c0baf34ddc1bf6f65460e6212f963
Alex Firestone <alexfirestone@Kaelyns-iPhone.localdomain>
2020-10-12 21:50:22 -0700

    setting up our react app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-west-1",
+      BUCKET: "notes-app-tutorial-18"
+    },
+    apiGateway: {
+      REGION: "us-west-1",
+      URL: "https://88bth5u1g6.execute-api.us-west-1.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-east-1",
+      USER_POOL_ID: "us-east-1_9WJagoi7O",
+      APP_CLIENT_ID: "29cnsnpshjj1hlp0qj695qtd2f",
+      IDENTITY_POOL_ID: "us-east-1:b7dba417-6839-4f8d-bcc1-baa8270a92d5"
+    }
+  };
